ef6cb1af31096fe082965ad2cb54c941134f8de9
Protanker25 <Protanker25@gmail.com>
2020-11-25 15:48:16 +1000

    Какие то изменения

--- NovelCore Creation Kit/Episode.cs
+++ NovelCore Creation Kit/Episode.cs
@@ -12,7 +12,7 @@ namespace NovelCore
         public string Name { get; set; } //Имя сцены
         public string[] UsedBackgrounds { get; set; } //Набор используемых фонов
         public Dictionary<string, string[]> UsedSprites { get; set; } //<Имя персонажа, набор спрайтов
-        public Scene[] Scenes { get; set; }//
+        public Scene[] Scenes { get; set; }
 
         public Scene this[int number]
         {

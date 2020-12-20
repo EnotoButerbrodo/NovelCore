a402d735b34dcba282b5289a138ca2dd30d69613
Protanker25 <Protanker25@gmail.com>
2020-12-20 22:56:18 +1000

    Setup в отдельный файл

--- NovelCore/MainWindow.xaml.cs
+++ NovelCore/MainWindow.xaml.cs
@@ -26,7 +26,6 @@ namespace NovelCore
         {
             InitializeComponent();
             MainScene.Children.Add(BackgroundImage.Spot); 
-            //InitialClickSpot();
             TextBox.TextAnimationComplete += delegate ()
             {
                 IsSceneComplete = true;
@@ -90,61 +89,7 @@ namespace NovelCore
         }
         
 
-        #region SceneSetup
-        void SetupBackgroud(BackgroundArgs args)
-        {
-            BackgroundImage.SetImage(Backgrounds[args.Background]);
-        }
-        void SetupBackgroundAnimation(AnimationSettings args)
-        {
-            //SceneBackground.Move(args);
-        }
-        void SetupCharactersAppearance(CharacterArgs[] args) 
-        {
-            foreach(var arg in args)
-            {
-                Characters[arg.Character].SetAppearance(arg.Sprite);
-
-            }
-        }
-        void SetupSceneAnimation(Scene scene)
-        {
-            SceneAnimation = new Storyboard();
-            
-            SceneAnimation.Completed += new EventHandler((o, e) =>
-            {
-                TextBox.SetText(scene.Text[0]);
-            });
-
-            void AddToStroyboard(Grid Spot, AnimationSettings args)
-            {
-                if (args.EndPoint.X != null)
-                {
-                    DoubleAnimation anim_X = new DoubleAnimation((double)args.EndPoint.X, TimeSpan.FromMilliseconds(args.Speed));
-                    Storyboard.SetTarget(anim_X, Spot);
-                    Storyboard.SetTargetProperty(anim_X, new PropertyPath("(Canvas.Left)"));
-                    SceneAnimation.Children.Add(anim_X);
-                }
-                if (args.EndPoint.Y != null)
-                {
-                    DoubleAnimation anim_Y = new DoubleAnimation((double)args.EndPoint.Y, TimeSpan.FromMilliseconds(args.Speed));
-                    Storyboard.SetTarget(anim_Y, Spot);
-                    Storyboard.SetTargetProperty(anim_Y, new PropertyPath("(Canvas.Bottom)"));
-                    SceneAnimation.Children.Add(anim_Y);
-                }
-            }
-            //Персонажи
-            foreach (var charAnim in scene.CharactersConfig)
-            {
-                if (charAnim.AnimationConfig.Timing == AnimationTiming.AtBegin)
-                    AddToStroyboard(Characters[charAnim.Character].Spot,
-                    charAnim.AnimationConfig);
-            }
-            //Задний фон
-        }
-
-
-            #endregion
+       
 
         #region Audio
             void StartPlayAudio(AudioArgs args)
---
+++ NovelCore/SetupResources.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using Ionic.Zip;
+using NAudio.Wave;
+using Plugin.SimpleAudioPlayer;
+
+namespace NovelCore
+{
+
+    public partial class MainWindow : Window
+    {
+        #region SceneSetup
+        void SetupBackgroud(BackgroundArgs args)
+        {
+            BackgroundImage.SetImage(Backgrounds[args.Background]);
+        }
+        void SetupBackgroundAnimation(AnimationSettings args)
+        {
+            //SceneBackground.Move(args);
+        }
+        void SetupCharactersAppearance(CharacterArgs[] args)
+        {
+            foreach (var arg in args)
+            {
+                Characters[arg.Character].SetAppearance(arg.Sprite);
+
+            }
+        }
+        void SetupSceneAnimation(Scene scene)
+        {
+            SceneAnimation = new Storyboard();
+
+            SceneAnimation.Completed += new EventHandler((o, e) =>
+            {
+                TextBox.SetText(scene.Text[0]);
+            });
+
+            void AddToStroyboard(Grid Spot, AnimationSettings args)
+            {
+                if (args.EndPoint.X != null)
+                {
+                    DoubleAnimation anim_X = new DoubleAnimation((double)args.EndPoint.X, TimeSpan.FromMilliseconds(args.Speed));
+                    Storyboard.SetTarget(anim_X, Spot);
+                    Storyboard.SetTargetProperty(anim_X, new PropertyPath("(Canvas.Left)"));
+                    SceneAnimation.Children.Add(anim_X);
+                }
+                if (args.EndPoint.Y != null)
+                {
+                    DoubleAnimation anim_Y = new DoubleAnimation((double)args.EndPoint.Y, TimeSpan.FromMilliseconds(args.Speed));
+                    Storyboard.SetTarget(anim_Y, Spot);
+                    Storyboard.SetTargetProperty(anim_Y, new PropertyPath("(Canvas.Bottom)"));
+                    SceneAnimation.Children.Add(anim_Y);
+                }
+            }
+            //Персонажи
+            foreach (var charAnim in scene.CharactersConfig)
+            {
+                if (charAnim.AnimationConfig.Timing == AnimationTiming.AtBegin)
+                    AddToStroyboard(Characters[charAnim.Character].Spot,
+                    charAnim.AnimationConfig);
+            }
+            //Задний фон
+        }
+
+
+        #endregion
+    }
+}
